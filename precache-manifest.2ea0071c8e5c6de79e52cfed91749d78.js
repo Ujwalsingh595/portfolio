37d8fb55e941a8757488ff83dc801cf423c4f520
Ujwalsingh595 <ujwalsingh595@gmail.com>
2020-07-09 13:06:05 +0530

    Updates

---
+++ precache-manifest.2ea0071c8e5c6de79e52cfed91749d78.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccbd4ade8a767ce8a0d446509f6396f2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3df6d26d051eb400f57d",
+    "url": "/portfolio/static/css/main.0c0c0542.chunk.css"
+  },
+  {
+    "revision": "33e5dd6cd7e410be08de",
+    "url": "/portfolio/static/js/2.fadb4be8.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/portfolio/static/js/2.fadb4be8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3df6d26d051eb400f57d",
+    "url": "/portfolio/static/js/main.3b35a4f1.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "7c8e5f50725885d51b72b45164274e41",
+    "url": "/portfolio/static/media/avatar.7c8e5f50.png"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/portfolio/static/media/coffee-apple.2dd213aa.jpg"
+  }
+]);
